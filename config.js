97f695af97b145be33f54c891b87fbf4a2646234
thehoangbka1997 <49914551+thehoangbka1997@users.noreply.github.com>
2022-10-10 22:31:57 +0900

    ok

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 ハロー ! 🖐🖐🖐',
+    introDesc: `聞きたいことがあるけど、素直に教えてね`,
+    btnIntro: 'OK',
+    title: '僕のことが好き？ 😙',
+    desc: '答えられないってことは僕のことが大好きだよね :> ',
+    btnYes: 'はい、好きです 💖💖💖',
+    btnNo: '💔 そんなことねーぞ ! 💔 ',
+    question:'なぜ僕のことが好きなの 😊😊',
+    reasonPlaceholder: '理由',
+    btnReply: 'ホアン君へ ! 💌💌',
+    reply: 'ホアン君がカッコイイから 😊😊',
+    mess: '分かった。サンキュー 🥰',
+    messDesc: '💬💬 僕にメッセンジしてね. 💬💬',
+    btnAccept: 'オッケー 💖',
+    messLink: 'http://m.me/thehoang.nguyen.37'
+}
